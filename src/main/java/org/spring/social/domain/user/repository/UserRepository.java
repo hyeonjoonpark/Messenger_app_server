4af2c5786bf7124e07add4d169e960745bbe0ec1
hyeonjoonpark <pjjoon1379@gmail.com>
2024-02-01 21:46:26 +0900

    P6Spyformatter

---
+++ src/main/java/org/spring/social/domain/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.spring.social.domain.user.repository;
+
+import org.spring.social.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
